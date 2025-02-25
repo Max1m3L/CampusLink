3f0d4217c2fbc80e5ef5d204a3d44ea626114203
Max <clashshow2000@gmail.com>
2025-02-25 14:19:10 +0500

    small fix

--- backend/src/main/java/com/maxlvshv/backend/repository/JobRepository.java
+++ backend/src/main/java/com/maxlvshv/backend/repository/JobRepository.java
@@ -3,7 +3,6 @@ package com.maxlvshv.backend.repository;
 import com.maxlvshv.backend.entity.Job;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
